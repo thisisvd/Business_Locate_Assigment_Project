ee8f402c1f5cae01912bf5bdb6a860582c8de7ea
Vimal Dubey <vimaldubey123@outlook.com>
2021-09-16 21:33:12 +0530

    Business Login added

---
+++ app/src/main/java/com/vdcodeassociate/businesslocateassigmentproject/LoginFragment.kt
@@ -0,0 +1,39 @@
+package com.vdcodeassociate.businesslocateassigmentproject
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [LoginFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
+
+        view?.findViewById<Button>(R.id.login_sendotp_button)
+            ?.setOnClickListener() {
+                findNavController().navigate(R.id.action_loginFragment_to_OTPVerificationFragment3)
+            }
+
+        return view
+
+    }
+
+}
