e320a85e3a5e5b250740a06cfe41f065852e150c
Vimal Dubey <vimaldubey123@outlook.com>
2021-12-30 21:10:50 +0530

    login update...

--- app/src/main/java/com/vdcodeassociate/businesslocateassigmentproject/ui/fragments/BusinessLoginFragment.kt
+++ app/src/main/java/com/vdcodeassociate/businesslocateassigmentproject/ui/fragments/BusinessLoginFragment.kt
@@ -1,60 +1,100 @@
 package com.vdcodeassociate.businesslocateassigmentproject.ui.fragments
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
+import androidx.navigation.fragment.findNavController
 import com.vdcodeassociate.businesslocateassigmentproject.R
+import com.vdcodeassociate.businesslocateassigmentproject.databinding.FragmentBusinessLoginBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class BusinessLoginFragment : Fragment(R.layout.fragment_business_login) {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [BusinessLoginFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class BusinessLoginFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    // viewBinding
+    private lateinit var binding: FragmentBusinessLoginBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentBusinessLoginBinding.bind(view)
+
+        binding.apply {
+
+            // go to sign up fragment
+            signUp.setOnClickListener{
+                findNavController().navigate(R.id.action_businessLoginFragment_to_businessSignUpFragment)
+            }
+
+            // login pressed
+            loginButton.setOnClickListener {
+                login()
+            }
+
+        }
+
+    }
+
+    // user exist firebase
+    private fun loginViaFirebase(){
+
+    }
+
+    // proceed login
+    private fun login(){
+        if(!isTextEmpty()){
+            Toast.makeText(requireContext(),"Logined",Toast.LENGTH_SHORT).show()
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_business_login, container, false)
+    // checks for empty texts
+    private fun isTextEmpty(): Boolean {
+
+        allTextEmptyListener()
+
+        var isTextEmpty = false
+
+        binding.apply {
+
+            if (email.text.isNullOrEmpty()) {
+                isTextEmpty = true
+                emailLayout.error = "Registered email required!"
+            }
+
+            if (password.text.isNullOrEmpty()) {
+                isTextEmpty = true
+                passwordLayout.error = "Registered password required!"
+            }
+
+        }
+
+        return isTextEmpty
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment BusinessLoginFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            BusinessLoginFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    // all text empty reset
+    private fun allTextEmptyListener(){
+        binding.apply {
+
+            email.addTextChangedListener{
+                if(it.isNullOrEmpty()){
+                    emailLayout.error = "Registered email required!"
+                } else {
+                    emailLayout.error = null
                 }
             }
+
+            password.addTextChangedListener{
+                if(it.isNullOrEmpty()){
+                    passwordLayout.error = "Registered password required!"
+                } else {
+                    passwordLayout.error = null
+                }
+            }
+
+        }
     }
+
 }
--- app/src/main/java/com/vdcodeassociate/businesslocateassigmentproject/ui/fragments/BusinessSignUpFragment.kt
+++ app/src/main/java/com/vdcodeassociate/businesslocateassigmentproject/ui/fragments/BusinessSignUpFragment.kt
@@ -5,56 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RelativeLayout
+import androidx.navigation.fragment.findNavController
 import com.vdcodeassociate.businesslocateassigmentproject.R
+import com.vdcodeassociate.businesslocateassigmentproject.databinding.FragmentBusinessSignUpBinding
+import kotlin.math.log
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class BusinessSignUpFragment : Fragment(R.layout.fragment_business_sign_up) {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [BusinessSignUpFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class BusinessSignUpFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    // viewBinding
+    private lateinit var binding: FragmentBusinessSignUpBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentBusinessSignUpBinding.bind(view)
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_business_sign_up, container, false)
-    }
+        binding.apply {
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment BusinessSignUpFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            BusinessSignUpFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+            login.setOnClickListener{
+                findNavController().navigate(R.id.action_businessSignUpFragment_to_businessLoginFragment)
             }
+
+        }
+
     }
+
 }
